b04a031f05f1f03700bda2bad066dc3305d7d1e8
snow <582658211@qq.com>
2024-04-29 17:19:20 +0800

    chore: adjust style

--- apps/client/components/designer/simulator/board.tsx
+++ apps/client/components/designer/simulator/board.tsx
@@ -2,17 +2,38 @@ function Board() {
   const boardRef = ref<HTMLDivElement | null>(null);
   const contentRef = ref<HTMLDivElement | null>(null);
   const translate = shallowReactive({ x: 0, y: 0 })
-  
+
+
+  function setTranslate(deltaX: number, deltaY: number) {
+    const moveX = deltaX !== 0;
+    const moveY = deltaY !== 0;
+
+    if (moveX) {
+      if (Number(deltaX) > 0) {
+        translate.x += deltaX;
+      } else {
+        translate.x -= Math.abs(deltaX);
+      }
+    }
+
+    if (moveY) {
+      if (Number(deltaY) > 0) {
+        translate.y += deltaY;
+      } else {
+        translate.y -= Math.abs(deltaY);
+      }
+    }
+  }
+
 
   function bindUpdateBoardEvent() {
     const dom = boardRef.value;
     if (!dom) { return }
 
-
     dom.onmousedown = function (evt: MouseEvent) {
       evt.preventDefault();
 
-      if(contentRef.value?.contains(evt.target as HTMLElement)){
+      if (contentRef.value?.contains(evt.target as HTMLElement)) {
         return;
       }
 
@@ -27,35 +48,15 @@ function Board() {
         const currentX = evt.pageX;
         const currentY = evt.pageY;
 
-        // 计算从上一次到这一次的移动距离
         const deltaX = currentX - lastX;
         const deltaY = currentY - lastY;
 
-        // 对历史坐标进行更新
         lastX = currentX;
         lastY = currentY;
 
-        const moveX = deltaX !== 0;
-        const moveY = deltaY !== 0;
-
-        if(moveX) {
-          if(Number(deltaX) > 0){
-            translate.x += deltaX;
-          } else {
-            translate.x -= Math.abs(deltaX);
-          }
-        }
-
-        if(moveY) {
-          if(Number(deltaY) > 0){
-            translate.y += deltaY;
-          } else {
-            translate.y -= Math.abs(deltaY);
-          }
-        }
+        setTranslate(deltaX,deltaY);
       };
 
-      // 解绑事件
       document.onmouseup = function () {
         document.onmousemove = null;
         document.onmouseup = null;
@@ -70,8 +71,8 @@ function Board() {
       class="w-full h-full absolute top-[50%] left-[50%] translate-[-50%] flex items-center justify-center"
       ref={boardRef}
     >
-      <div 
-        class={`w-[375px] h-[667px] shadow-custom  bg-base-100 absolute cursor-auto`} 
+      <div
+        class={`w-[375px] h-[667px] shadow-custom  bg-base-100 absolute cursor-auto`}
         style={`transform:translate(${translate.x}px,${translate.y}px)`}
         ref={contentRef}
       >
--- apps/client/components/designer/simulator/index.tsx
+++ apps/client/components/designer/simulator/index.tsx
@@ -6,34 +6,23 @@ function Simulator() {
   const el = ref<HTMLDivElement | null>(null)
   const size = reactive(useElementSize(el))
 
+  const rulerProps = computed(() => ({
+    height: size.height,
+    width: size.width,
+    offsetX: 0,
+    offsetY: 0,
+    scale: 1
+  }))
+
   return (
     <div
       class="m-[15px] flex-1 relative overflow-hidden relative cursor-pointer main-height"
       ref={el}
     >
-
+      <Ruler mode="horizontal" {...rulerProps.value} />
+      <Ruler mode="vertical"  {...rulerProps.value} />
       <Board />
-
       <Tools />
-
-      
-      <Ruler
-        mode="horizontal"
-        width={size.width}
-        height={size.height}
-        offsetX={0}
-        offsetY={0}
-        scale={1}
-      />
-
-      <Ruler
-        mode="vertical"
-        width={size.width}
-        height={size.height}
-        offsetX={0}
-        offsetY={0}
-        scale={1}
-      />
     </div>
   )
 }
